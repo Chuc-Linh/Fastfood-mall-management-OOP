d3210113a10b9e031e694f5108d61f4b8551958e
AerieL114 <nguyentuanlongqng@gmail.com>
2025-11-06 11:27:05 +0700

    update class taikhoan
    tai khoan extends nguoi

--- Nguoi/taiKhoan.java
+++ Nguoi/taiKhoan.java
@@ -1,13 +1,15 @@
 package Nguoi;
 
 import Interface.Itaikhoan;
+import sanpham.HOADON;
 
-public class taiKhoan implements Itaikhoan {
+public class taiKhoan extends nguoi implements Itaikhoan {
     private String tenTK;
     private String matKhau;
     private int SLM;
 
-    public taiKhoan(String tenTK, String matKhau) {
+    public taiKhoan(String tenTK, String matKhau, String maDG, String ngaysinh, String GT) {
+        super(maDG, ngaysinh, GT);
         this.tenTK = tenTK;
         this.matKhau = matKhau;
         this.SLM = 0;
@@ -23,8 +25,8 @@ public class taiKhoan implements Itaikhoan {
 
     @Override
     public void dangnhap(String tk, String mk) {
-        if (this.tenTK.equals(tenTK) && this.matKhau.equals(matKhau)) {
-            System.out.println("Đăng nhập thành công!");
+        if (this.tenTK.equals(tk) && this.matKhau.equals(mk)) {
+            System.out.println("Đăng nhập thành công!\n");
         } else {
             System.out.println("Sai tên tài khoản hoặc mật khẩu!");
         }
@@ -32,13 +34,17 @@ public class taiKhoan implements Itaikhoan {
 
     @Override
     public void dangxuat() {
-        System.out.println("Đã đăng xuất tài khoản " + tenTK);
+        System.out.println("Đã đăng xuất tài khoản " + tenTK.toUpperCase() + "\n");
     }
 
     public int getSLM() { return SLM;}
 
     public void muaHang() {
         SLM++;
+    }
+
+    public void xuat() {
+        super.xuat();
         System.out.println("Đã mua hàng, tổng số lần mua: " + SLM);
     }
 
--- main.java
+++ main.java
@@ -6,9 +6,9 @@ public class main {
         khachhang kh = new khachhang("DG002", "02/02/1992","nu", "KH001", "Nguyen Thi A");
 //        khachhang kh2 = new khachhang("DG004", "02/02/1998","na", "KH002", "Nguyen Van B");
 
-        taiKhoan tk = new taiKhoan("long", "123");
+        taiKhoan tk = new taiKhoan("long", "123", "DG001", "02-02-2003", "m");
         tk.dangnhap("long", "123");
-        ((taiKhoan) tk).muaHang();
+        tk.xuat();
         tk.dangxuat();
 
         THUCAN com1 = new COM("C01", "Com tam", 30000, 1, 0.1, "Thit");
@@ -17,7 +17,13 @@ public class main {
         COMBO combo1 = new COMBO("CB01", "Combo Com + Nuoc", 0, 1, 0.1, com1, nuoc1);
 
         // Tao hoa don
-        HOADON hd = new HOADON();
+        HOADON hd = new HOADON(tk);
+        hd.themSanPham(combo1);
+        hd.xuatHoaDon();
+
+        tk.dangnhap("long", "123");
+        tk.xuat();
+        tk.dangxuat();
 
     }
 }
